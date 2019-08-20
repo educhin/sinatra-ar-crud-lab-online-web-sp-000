f1e0c714cab0487fdc2816866f359a96620e41c6
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-20 19:38:33 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 #Placeholder for a model
+require 'active_record'
 
 class Article < ActiveRecord::Base
+  attr_reader :title, :content
+
+  def initialize(params)
+    @title = params["title"]
+    @content = params["content"]
+  end
 end
