bb880a5712370506d866603e00f9cad13df41fc5
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-20 19:45:43 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,16 @@ require 'active_record'
 class Article < ActiveRecord::Base
   attr_reader :title, :content
 
+  @@all = []
+
   def initialize(params)
     @title = params["title"]
     @content = params["content"]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
