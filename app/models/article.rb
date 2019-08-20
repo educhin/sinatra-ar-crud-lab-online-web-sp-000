e65d7e34ade09453acf3887d9da91574a1d919ee
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-20 19:59:02 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,8 @@ class Article < ActiveRecord::Base
   @@all = []
 
   def initialize(params)
-    @title = params["title"]
-    @content = params["content"]
+    @title = params
+    @content = params
 
     @@all << self
   end
