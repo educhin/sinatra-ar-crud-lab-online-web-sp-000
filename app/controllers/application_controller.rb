7f63722e8534d710b7e51b56c8c820c993696dad
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-20 20:14:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
 
   # create
   post "/articles" do
-    @article = Article.create({:title => params[:title], :content => params[:content]})
+    @article = Article.create(params)
     redirect to "/articles/#{ @article.id }"
   end
 
